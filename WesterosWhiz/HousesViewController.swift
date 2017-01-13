e1d56f5657c37a78e4713132a315d0159cfbeaac
ezaman <ehsanmzaman@gmail.com>
2017-01-13 12:05:07 -0500

    cleaning code

--- WesterosWhiz/HousesViewController.swift
+++ WesterosWhiz/HousesViewController.swift
@@ -131,14 +131,4 @@ class HousesViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
